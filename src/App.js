13cf244ddefce04d6901ab36743a3e8fca181c76
Aya Yahia <ayayahia01@gmail.com>
2024-01-18 17:49:56 +0200

    WIP - add routes in the react task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
 import Productlist from "./components/Productlist";
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
- 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about/*" element={<About />} />
-      
-    </Routes>
-
-      <Slider />
-      <Productlist />
-      <About/>
-     
+        <Route
+          path="/"
+          element={
+            <>
+              <Slider /> 
+              <Productlist />
+            </>
+          }
+        />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
 return(<>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <Link className="navbar-brand" to="/"/>logo
+    <link/>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,7 +16,7 @@ return(<>
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">About</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Pricing</a>
